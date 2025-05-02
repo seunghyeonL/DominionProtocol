46774e85b2ec6a8b698d49be7448c9e4fffbde61
eomcri <jihun.eomcri@gmail.com>
2025-05-02 21:26:29 +0900

    [add] 기본 프로젝트 파일 추가

---
+++ DominionProtocol/Source/DominionProtocol/DominionProtocol.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DominionProtocol : ModuleRules
+{
+	public DominionProtocol(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ DominionProtocol/Source/DominionProtocol/DominionProtocolGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DominionProtocolGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADominionProtocolGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADominionProtocolGameMode();
+};
+
+
+
