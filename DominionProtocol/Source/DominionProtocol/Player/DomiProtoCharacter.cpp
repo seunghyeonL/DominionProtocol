e662440b4c728af0885e88c11c70f63186ca6ddb
seunghyeonL <hkr0416@gmail.com>
2025-05-07 19:00:46 +0900

    [add] Editor Dev directory, DomiProtoCharacter cpp Files

---
+++ DominionProtocol/Source/DominionProtocol/Player/DomiProtoCharacter.cpp
@@ -0,0 +1,210 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DomiProtoCharacter.h"
+#include "Util/DebugHelper.h"
+#include "Engine/LocalPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "Util/BattleDataTypes.h"
+#include "Components/ControlComponent/Player/PlayerControlComponent.h"
+#include "Components/ControlComponent/Player/States/PlayerControlState.h"
+#include "Components/StatusComponent/Player/PlayerStatusComponent.h"
+
+//////////////////////////////////////////////////////////////////////////
+// ADomiProtoCharacter
+
+ADomiProtoCharacter::ADomiProtoCharacter()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+		
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+	// Battle Components
+	ControlComponent = CreateDefaultSubobject<UPlayerControlComponent>(TEXT("ControlComponent"));
+	StatusComponent = CreateDefaultSubobject<UPlayerStatusComponent>(TEXT("StatusComponent"));
+	
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void ADomiProtoCharacter::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
+
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void ADomiProtoCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	InputComponent = PlayerInputComponent;
+	
+	if (ControlComponent->bIsComponentReady)
+	{
+		BindInputFunctions();
+	}
+	ControlComponent->OnComponentReady.BindUObject(this, &ADomiProtoCharacter::BindInputFunctions);
+}
+
+void ADomiProtoCharacter::BindInputFunctions()
+{
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent)) {
+		// Moving
+		if (IsValid(MoveAction))
+		{
+			EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, ControlComponent.Get(), &UPlayerControlComponent::Move);	
+		}
+		
+		// Looking
+		if (IsValid(LookAction))
+		{
+			EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, ControlComponent.Get(), &UPlayerControlComponent::Look);
+		}
+
+		// Dash
+		if (IsValid(DashAction))
+		{
+			EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Started, ControlComponent.Get(), &UPlayerControlComponent::Dash);
+		}
+
+		// Dash
+		if (IsValid(SprintAction))
+		{
+			EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Started, ControlComponent.Get(), &UPlayerControlComponent::Sprint);
+		}
+
+		// Parry
+		if (IsValid(ParryAction))
+		{
+			EnhancedInputComponent->BindAction(ParryAction, ETriggerEvent::Started, ControlComponent.Get(), &UPlayerControlComponent::Parry);
+		}
+
+		// BaseAttack
+		if (IsValid(BaseAttackAction))
+		{
+			EnhancedInputComponent->BindAction(BaseAttackAction, ETriggerEvent::Started, ControlComponent.Get(), &UPlayerControlComponent::BaseAttack);
+		}
+
+		// WeaponSkill
+		if (IsValid(WeaponSkillAction))
+		{
+			EnhancedInputComponent->BindAction(WeaponSkillAction, ETriggerEvent::Started, ControlComponent.Get(), &UPlayerControlComponent::WeaponSkill);
+		}
+
+		// MagicSkill
+		if (IsValid(MagicSkillAction))
+		{
+			EnhancedInputComponent->BindAction(MagicSkillAction, ETriggerEvent::Started, ControlComponent.Get(), &UPlayerControlComponent::MagicSkill);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+}
+
+void ADomiProtoCharacter::OnAttacked_Implementation(const FAttackData& AttackData)
+{
+	IDamagable::OnAttacked_Implementation(AttackData);
+
+	if (!IsValid(ControlComponent))
+	{
+		Debug::PrintError(TEXT("ADomiProtoCharacter::OnAttacked : ControlComponent is not valid"));
+		return;
+	}
+
+	if (!IsValid(StatusComponent))
+	{
+		Debug::PrintError(TEXT("ADomiProtoCharacter::OnAttacked : StatusComponent is not valid"));
+		return;
+	}
+	
+	float CurrentHealth = StatusComponent->GetStat(StatTags::Health);
+	StatusComponent->SetHealth(CurrentHealth - AttackData.Damage);
+
+	LaunchCharacter(AttackData.LaunchVector, true, true);
+
+	for (FEffectData EffectData : AttackData.Effects)
+	{
+		auto [EffectTag, Magnitude, Duration] = EffectData;
+
+		if (EffectTag.MatchesTag(FGameplayTag::RequestGameplayTag(TEXT("Effect.Control"))))
+		{
+			ControlComponent->ActivateControlEffectWithDuration(EffectTag, Duration);
+		}
+		else
+		{
+			StatusComponent->ActivateStatusEffectWithDuration(EffectTag, Magnitude, Duration);
+		}
+	}
+}
+
+FGameplayTagContainer ADomiProtoCharacter::GetActiveControlEffectTags_Implementation()
+{
+	IEffectReceivable::GetActiveControlEffectTags_Implementation();
+
+	if (!IsValid(ControlComponent))
+	{
+		Debug::PrintError(TEXT("ADomiProtoCharacter::GetActiveControlEffectTags : ControlComponent is not valid"));
+		return FGameplayTagContainer();
+	}
+
+	return ControlComponent->GetActiveControlEffects();
+}
+
+FGameplayTagContainer ADomiProtoCharacter::GetActiveStatusEffectTags_Implementation()
+{
+	IEffectReceivable::GetActiveStatusEffectTags_Implementation();
+
+	if (!IsValid(StatusComponent))
+	{
+		Debug::PrintError(TEXT("ADomiProtoCharacter::GetActiveControlEffectTags : ControlComponent is not valid"));
+		return FGameplayTagContainer();
+	}
+
+	return StatusComponent->GetActiveStatusEffectTags();
+}
---
+++ DominionProtocol/Source/DominionProtocol/Player/DomiProtoCharacter.h
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Damagable.h"
+#include "EffectReceivable.h"
+#include "DomiProtoCharacter.generated.h"
+
+class UPlayerStatusComponent;
+class UPlayerControlComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+struct FAttackData;
+
+UCLASS()
+class DOMINIONPROTOCOL_API ADomiProtoCharacter :
+public ACharacter, public IDamagable, public IEffectReceivable
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPlayerControlComponent> ControlComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPlayerStatusComponent> StatusComponent;
+	
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	// Dash Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* DashAction;
+	
+	// Sprint Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* SprintAction;
+
+	// Parry Input Action 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* ParryAction;
+	
+	// BaseAttack Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* BaseAttackAction;
+
+	// WeaponSkill Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* WeaponSkillAction;
+
+	// MagicSkill Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MagicSkillAction;
+	
+public:
+	ADomiProtoCharacter();
+
+protected:
+	// Bind After ControlComponent Ready
+	void BindInputFunctions();
+	
+	virtual void NotifyControllerChanged() override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	UFUNCTION(BlueprintCallable, Category="Components")
+	FORCEINLINE	UPlayerControlComponent* GetPlayerControlComponent() const { return ControlComponent; }
+
+	UFUNCTION(BlueprintCallable, Category="Components")
+	FORCEINLINE UPlayerStatusComponent* GetPlayerStatusComponent() const { return StatusComponent; }
+
+	// Damagable
+	virtual void OnAttacked_Implementation(const FAttackData& AttackData) override;
+
+	// EffectReceivable
+	virtual FGameplayTagContainer GetActiveControlEffectTags_Implementation() override;
+	virtual FGameplayTagContainer GetActiveStatusEffectTags_Implementation() override;
+};
