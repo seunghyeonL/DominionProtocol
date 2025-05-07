28f64a75de17e8b1592ca84fa3f8923ee2323523
ChaeKH <rbgur211@naver.com>
2025-05-07 14:25:36 +0900

    [add] 프레임워크 기본 파일 추가

---
+++ DominionProtocol/Source/DominionProtocol/DominionFramework/GameMode/BaseGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOMINIONPROTOCOL_API ABaseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
---
+++ DominionProtocol/Source/DominionProtocol/DominionFramework/GameState/BaseGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "BaseGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOMINIONPROTOCOL_API ABaseGameState : public AGameState
+{
+	GENERATED_BODY()
+};
