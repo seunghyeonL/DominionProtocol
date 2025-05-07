68667d234ce8d0bd675b8b83c7d532cf40a810b6
ChaeKH <rbgur211@naver.com>
2025-05-07 15:47:36 +0900

    [add] 프로토타입 : 레벨2 게임모드 파일 추가

---
+++ DominionProtocol/Source/DominionProtocol/DominionFramework/GameMode/ProtoLevel2GameMode.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProtoLevel2GameMode.h"
+
+#include "Util/DebugHelper.h"
+
+AProtoLevel2GameMode::AProtoLevel2GameMode()
+{
+}
