f45767d0ec43d333d0351203f388239d1469c469
eomcri <jihun.eomcri@gmail.com>
2025-05-12 18:09:49 +0900

    [update] Set Boss ID(prototype code)

--- Source/DominionProtocol/AI/ProtoBoss/ProtoBossEnemy.cpp
+++ Source/DominionProtocol/AI/ProtoBoss/ProtoBossEnemy.cpp
@@ -9,6 +9,7 @@ AProtoBossEnemy::AProtoBossEnemy()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	BossID = 0; // 프로토타입 코드
 }
 
 // Called when the game starts or when spawned
