12a57dd91900918f8ca6f788155beea8db661eb3
KimLink <Linkedcirrus93@gmail.com>
2025-05-13 18:34:18 +0900

    [Add] OptionWidget // Changed Display Resolution, Sound Volumes Gauge and Editable Text

---
+++ Source/DominionProtocol/UI/UITitle/DomiOptionWidget.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DomiOptionWidget.generated.h"
+
+
+UCLASS()
+class DOMINIONPROTOCOL_API UDomiOptionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
