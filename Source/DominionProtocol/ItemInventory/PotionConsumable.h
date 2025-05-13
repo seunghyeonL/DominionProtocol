6b8173e1374ba697fb1a89e49f6cc5b05ccbf109
ChungSH <kevin990114@gmail.com>
2025-05-13 14:10:45 +0900

    [add] Consumable Item Classes

---
+++ Source/DominionProtocol/ItemInventory/PotionConsumable.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemInventory/BaseItem.h"
+#include "PotionConsumable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOMINIONPROTOCOL_API APotionConsumable : public ABaseItem
+{
+	GENERATED_BODY()
+	
+};
