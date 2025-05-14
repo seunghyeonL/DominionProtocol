96e73029b8efd658ff7bb33c7ffcb3fcad0f7016
KimLink <Linkedcirrus93@gmail.com>
2025-05-14 13:04:36 +0900

    [Update] Init Sound UI based Slider Value

--- Source/DominionProtocol/DomiFramework/GameInstance/SoundInstanceSubsystem.h
+++ Source/DominionProtocol/DomiFramework/GameInstance/SoundInstanceSubsystem.h
@@ -14,17 +14,7 @@ class DOMINIONPROTOCOL_API USoundInstanceSubsystem : public UGameInstanceSubsyst
 	GENERATED_BODY()
 	
 public:
-	void LoadSoundClass();
-	void LoadVolumeSettings();
-
-	void OnLevelChanged(UWorld* NewWorld);
-
-	//Save & Load
-	void LoadSaveData(const FSoundSubsystemData& SaveData);
-	FSoundSubsystemData GetSaveData() const;
-	
 	//Setter
-	FORCEINLINE void SetWorldCache(UWorld* NewWorld) { World = NewWorld; }
 	void SetMasterVolume(const float NewVolume);
 	void SetSFXVolume(const float NewVolume);
 	void SetBGMVolume(const float NewVolume);
