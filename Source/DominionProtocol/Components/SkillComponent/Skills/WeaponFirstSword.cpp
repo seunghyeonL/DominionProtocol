a214701c712957ef76d2a33af8764a73a793c3ba
Development-Floor <ymw1023@kakao.com>
2025-05-15 15:42:23 +0900

    [add] Add WeaponSkill
    - WeaponSkill 추가

---
+++ Source/DominionProtocol/Components/SkillComponent/Skills/WeaponFirstSword.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/SkillComponent/Skills/WeaponFirstSword.h"
+#include "Util/GameTagList.h"
+
+UWeaponFirstSword::UWeaponFirstSword()
+{
+	SkillTag = SkillTags::WeaponSkillSword1;
+}
