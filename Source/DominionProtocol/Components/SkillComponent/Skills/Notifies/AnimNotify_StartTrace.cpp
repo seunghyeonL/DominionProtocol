05447668ae2de6bcd47762cf85a08c1cad7de4bd
Development-Floor <ymw1023@kakao.com>
2025-05-16 08:28:44 +0900

    [add] Add AnimNotify
    - 애니메이션 노티파이 추가

---
+++ Source/DominionProtocol/Components/SkillComponent/Skills/Notifies/AnimNotify_StartTrace.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/SkillComponent/Skills/Notifies/AnimNotify_StartTrace.h"
+#include "../Plugins/MissNoHit/Source/MissNoHit/Public/MnhTracerComponent.h"
+#include "Util/GameTagList.h"
+
+void UAnimNotify_StartTrace::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (IsValid(MeshComp))
+	{
+		AActor* Owner = MeshComp->GetOwner();
+
+		if (IsValid(Owner))
+		{
+			UMnhTracerComponent* MnhTracerComponent = Owner->FindComponentByClass<UMnhTracerComponent>();
+
+			if (IsValid(MnhTracerComponent))
+			{
+				FGameplayTagContainer TagContainer;
+				TagContainer.AddTag(ItemTags::BasicWeapon);
+
+				MnhTracerComponent->StartTracers(TagContainer);
+			}
+		}
+	}
+}
