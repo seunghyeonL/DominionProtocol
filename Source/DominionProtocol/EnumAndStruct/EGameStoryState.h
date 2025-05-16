290cde98f66bc67f52c978e482c00dd4a0c909c2
ChaeKH <rbgur211@naver.com>
2025-05-16 15:38:49 +0900

    [add] Add EGameStoryState enum class

---
+++ Source/DominionProtocol/EnumAndStruct/EGameStoryState.h
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGameStoryState.generated.h"
+
+UENUM(BlueprintType)
+enum class EGameStoryState : uint8
+{
+	Tutorial = 0 UMETA(DisplayName = "Tutorial")
+};
