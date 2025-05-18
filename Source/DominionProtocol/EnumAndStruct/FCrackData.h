a3b6e9a233071a05937bb18bbf7f399d0be61201
ChaeKH <rbgur211@naver.com>
2025-05-18 18:10:01 +0900

    [add] Add CrackDataStruct

---
+++ Source/DominionProtocol/EnumAndStruct/FCrackData.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FCrackData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCrackData
+{
+	GENERATED_BODY()
+
+	FCrackData()
+		:	bIsActivate(false),
+			RespawnLocation(FVector::ZeroVector),
+			RespawnRotation(FRotator::ZeroRotator)
+	{}
+
+	UPROPERTY()
+	FText CrackName;
+
+	UPROPERTY()
+	bool bIsActivate;
+	
+	UPROPERTY()
+	FVector RespawnLocation;
+
+	UPROPERTY()
+	FRotator RespawnRotation;
+};
+
+USTRUCT(BlueprintType)
+struct FCrackDataArray
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<FCrackData> CrackDataArray;
+};
