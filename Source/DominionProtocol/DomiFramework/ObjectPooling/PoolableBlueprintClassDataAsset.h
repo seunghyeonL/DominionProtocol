05bc7507ebd260b28d2e222cdffe836761bc124a
ChaeKH <rbgur211@naver.com>
2025-05-21 05:48:30 +0900

    [add] Add DataAsset for ObjectPolling
    오브젝트 풀링에서 블루프린트 클래스를 이용할 수 있도록 데이터 에셋에서 할당 가능하도록 추가함

---
+++ Source/DominionProtocol/DomiFramework/ObjectPooling/PoolableBlueprintClassDataAsset.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "PoolableBlueprintClassDataAsset.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPoolableActorBlueprintData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	FGameplayTag Tag;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> ActorClass;
+};
+
+UCLASS()
+class DOMINIONPROTOCOL_API UPoolableBlueprintClassDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FPoolableActorBlueprintData> PoolableBlueprintClassArray;
+};
