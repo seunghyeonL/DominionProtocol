e8bbc439c4d56b8fa7da78f087030d037148c975
ChaeKH <rbgur211@naver.com>
2025-05-21 02:52:51 +0900

    [bugfix] Fix Crack Bug
    Proto_Level1에서 Proto_Level2가 아닌 다른 테스트 레벨로 이동되는 문제 해결

--- Source/DominionProtocol/DomiFramework/GameInstance/WorldInstanceSubsystem.cpp
+++ Source/DominionProtocol/DomiFramework/GameInstance/WorldInstanceSubsystem.cpp
@@ -11,45 +11,42 @@ void UWorldInstanceSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	Super::Initialize(Collection);
 
 	// Level1 크랙 데이터(0번째 균열만)
-	{
-		FCrackDataArrayStruct Level1CrackArray;
-		FCrackData Level1CrackData;
+	FCrackDataArrayStruct Level1CrackArray;
+	FCrackData Level1CrackData;
+	FCrackDataArrayStruct Level2CrackArray;
+	FCrackData Level2CrackData;
 
-		Level1CrackData.OwnerLevelName = FString("Proto_Level1");
-		Level1CrackData.CrackIndex = 0;
-		Level1CrackData.CrackName = FText::FromString("Level1 Crack0");
-		Level1CrackData.bIsActivate = true;
-		Level1CrackData.RespawnLocation = FVector(-1860, -1510, 300);
-		Level1CrackData.RespawnRotation = FRotator(0, 0, 0);
-        
-		Level1CrackArray.CrackDataArray.Add(Level1CrackData);
-		CrackDataMap.Add(FString("Proto_Level1"), Level1CrackArray);
-		
-		Level1CrackArray.CrackDataArray[0].OwnerLevelName = FString("TestCrackLevel1");
-		CrackDataMap.Add(FString("TestCrackLevel1"), Level1CrackArray);
+	Level1CrackData.OwnerLevelName = FString("Proto_Level1");
+	Level1CrackData.CrackIndex = 0;
+	Level1CrackData.CrackName = FText::FromString("Level1 Crack0");
+	Level1CrackData.bIsActivate = true;
+	Level1CrackData.RespawnLocation = FVector(-1860, -1510, 300);
+	Level1CrackData.RespawnRotation = FRotator(0, 0, 0);
 
-		Level1CrackArray.CrackDataArray[0].OwnerLevelName = FString("TestLightWeightActorLevel");
-		CrackDataMap.Add(FString("TestLightWeightActorLevel"), Level1CrackArray);
-	}
-
+	Level1CrackArray.CrackDataArray.Add(Level1CrackData);
+	CrackDataMap.Add(FString("Proto_Level1"), Level1CrackArray);
+	
 	// Level2 크랙 데이터(0번째 균열만)
-	{
-		FCrackDataArrayStruct Level2CrackArray;
-		FCrackData Level2CrackData;
+	Level2CrackData.OwnerLevelName = FString("Proto_Level2");
+	Level2CrackData.CrackIndex = 0;
+	Level2CrackData.CrackName = FText::FromString("Level2 Crack0");
+	Level2CrackData.bIsActivate = true;
+	Level2CrackData.RespawnLocation = FVector(-1860, -1510, 300);
+	Level2CrackData.RespawnRotation = FRotator(0, 0, 0);
 
-		Level2CrackData.OwnerLevelName = FString("Proto_Level2");
-		Level2CrackData.CrackIndex = 0;
-		Level2CrackData.CrackName = FText::FromString("Level2 Crack0");
-		Level2CrackData.bIsActivate = true;
-		Level2CrackData.RespawnLocation = FVector(-1860, -1510, 300);
-		Level2CrackData.RespawnRotation = FRotator(0, 0, 0);
-        
-		Level2CrackArray.CrackDataArray.Add(Level2CrackData);
-		CrackDataMap.Add(FString("Proto_Level2"), Level2CrackArray);
+	Level2CrackArray.CrackDataArray.Add(Level2CrackData);
+	CrackDataMap.Add(FString("Proto_Level2"), Level2CrackArray);
 
-		Level2CrackArray.CrackDataArray[0].OwnerLevelName = FString("TestCrackLevel2");
-		CrackDataMap.Add(FString("TestCrackLevel2"), Level2CrackArray);
-	}
+
+	//다른 테스트 레벨들을 위한 코드
+	Level1CrackArray.CrackDataArray[0].OwnerLevelName = FString("TestCrackLevel1");
+	CrackDataMap.Add(FString("TestCrackLevel1"), Level1CrackArray);
+
+	Level1CrackArray.CrackDataArray[0].OwnerLevelName = FString("TestLightWeightActorLevel");
+	CrackDataMap.Add(FString("TestLightWeightActorLevel"), Level1CrackArray);
+	
+	Level2CrackArray.CrackDataArray[0].OwnerLevelName = FString("TestCrackLevel2");
+	CrackDataMap.Add(FString("TestCrackLevel2"), Level2CrackArray);
 }
 
 void UWorldInstanceSubsystem::Deinitialize()
