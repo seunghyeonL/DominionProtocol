b19211e3df7d4e7925a69ba102ba560930468922
ChungSH <kevin990114@gmail.com>
2025-05-22 20:08:42 +0900

    [fix] Ability Slot UI

--- Source/DominionProtocol/UI/UIInGame/DomiAbilitySlotsWidget.cpp
+++ Source/DominionProtocol/UI/UIInGame/DomiAbilitySlotsWidget.cpp
@@ -2,4 +2,46 @@
 
 
 #include "DomiAbilitySlotsWidget.h"
+#include "Components/SkillComponent/SkillComponent.h"
 
+void UDomiAbilitySlotsWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	BindUseWeaponSkillEvent();
+	BindUseMagicSkillEvent();
+}
+
+void UDomiAbilitySlotsWidget::BindUseWeaponSkillEvent()
+{
+	AActor* PlayerCharacter = GetOwningPlayerPawn();
+	if (PlayerCharacter)
+	{
+		auto* SkillComp = PlayerCharacter->FindComponentByClass<USkillComponent>();
+		if (SkillComp)
+		{
+			//SkillComp->OnUseWeaponSkill.AddUObject(this, &UDomiAbilitySlotsWidget::
+		}
+	}
+}
+
+void UDomiAbilitySlotsWidget::BindUseMagicSkillEvent()
+{
+	AActor* PlayerCharacter = GetOwningPlayerPawn();
+	if (PlayerCharacter)
+	{
+		auto* SkillComp = PlayerCharacter->FindComponentByClass<USkillComponent>();
+		if (SkillComp)
+		{
+			//SkillComp->OnUseWeaponSkill.AddUObject(this, &UDomiAbilitySlotsWidget::
+		}
+	}
+}
+
+void UDomiAbilitySlotsWidget::OnUseWeaponSkill()
+{
+}
+
+void UDomiAbilitySlotsWidget::OnUseMagicSkill()
+{
+}
