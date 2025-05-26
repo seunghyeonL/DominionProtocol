366c42241adc7653718893c9d6b0e4762d5628ce
ChaeKH <rbgur211@naver.com>
2025-05-26 12:58:40 +0900

    [fix] fix Crash Error in BaseGameState

--- Source/DominionProtocol/DomiFramework/GameState/BaseGameState.cpp
+++ Source/DominionProtocol/DomiFramework/GameState/BaseGameState.cpp
@@ -280,11 +280,10 @@ void ABaseGameState::InitializeZeroIndexCrackData(const FString CurrentLevelName
 
 	if (IsValid(WorldInstanceSubsystem))
 	{
-		if (WorldInstanceSubsystem->GetCurrentLevelName() != "Proto_Level1" ||
-			WorldInstanceSubsystem->GetCurrentLevelName() != "Proto_Level2" ||
-			WorldInstanceSubsystem->GetCurrentLevelName() != "TestCrackLevel1" ||
-			WorldInstanceSubsystem->GetCurrentLevelName() != "TestCrackLevel2" ||
-			WorldInstanceSubsystem->GetCurrentLevelName() != "Title_Level")
+		if (WorldInstanceSubsystem->GetCurrentLevelName() != "Proto_Level1" &&
+			WorldInstanceSubsystem->GetCurrentLevelName() != "Proto_Level2" &&
+			WorldInstanceSubsystem->GetCurrentLevelName() != "TestCrackLevel1" &&
+			WorldInstanceSubsystem->GetCurrentLevelName() != "TestCrackLevel2")
 		{
 			return;
 		}
