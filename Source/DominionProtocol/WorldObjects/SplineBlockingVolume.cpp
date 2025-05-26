df39339a7caedffffd57ab9b8238c367d8da716c
ChaeKH <rbgur211@naver.com>
2025-05-26 20:42:31 +0900

    [add] Spline Blocking Volume Spawner
    스플라인에 따라 BoxCollision을 생성시켜주는 BP_SplineBlockingVolume 제작

---
+++ Source/DominionProtocol/WorldObjects/SplineBlockingVolume.cpp
@@ -0,0 +1,93 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SplineBlockingVolume.h"
+
+#include "MovieSceneTracksComponentTypes.h"
+#include "Components/SplineComponent.h"
+#include "Components/BoxComponent.h"
+
+ASplineBlockingVolume::ASplineBlockingVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	RootComponent = SceneRoot;
+	
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+	SplineComponent->SetupAttachment(RootComponent);
+}
+
+void ASplineBlockingVolume::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASplineBlockingVolume::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	ClearBox();
+	CreateBox();
+}
+
+void ASplineBlockingVolume::ClearBox()
+{
+	for (UBoxComponent* Box : BoxComponents)
+	{
+		if (IsValid(Box))
+		{
+			Box->DestroyComponent();
+		}
+	}
+	BoxComponents.Empty();
+}
+
+void ASplineBlockingVolume::CreateBox()
+{
+	float SplineLength = SplineComponent->GetSplineLength();
+
+	float Distance = 0.f;
+	while (Distance < SplineLength)
+	{
+		float Curvature = CalculateCurvatureAtDistance(Distance);
+
+		float Spacing = FMath::Lerp(MaxSpacing, MinSpacing, FMath::Clamp(Curvature / CurvatureMax, 0.f, 1.f));
+
+		FVector Location = SplineComponent->GetLocationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+		FRotator Rotation = SplineComponent->GetRotationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+
+		// Box 컴포넌트 생성
+		UBoxComponent* NewBox = NewObject<UBoxComponent>(this);
+		NewBox->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+		NewBox->RegisterComponent();
+
+		NewBox->SetBoxExtent(CollisionBoxExtent);
+		NewBox->SetWorldLocation(Location);
+		NewBox->SetWorldRotation(Rotation);
+
+		NewBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		NewBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+
+		BoxComponents.Add(NewBox);
+
+		// 다음 생성 위치로 이동
+		Distance += Spacing;
+	}
+}
+
+float ASplineBlockingVolume::CalculateCurvatureAtDistance(float Distance) const
+{
+	const float Delta = 1.f;
+
+	FVector Tangent1 = SplineComponent->GetTangentAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+	FVector Tangent2 = SplineComponent->GetTangentAtDistanceAlongSpline(FMath::Min(Distance + Delta, SplineComponent->GetSplineLength()), ESplineCoordinateSpace::World);
+
+	FVector Derivative = (Tangent2 - Tangent1) / Delta;
+
+	float Speed = Tangent1.Size();
+	if (Speed < KINDA_SMALL_NUMBER) return 0.f;
+
+	float Curvature = Derivative.Size() / FMath::Pow(Speed, 3);
+	return Curvature;
+}
---
+++ Source/DominionProtocol/WorldObjects/SplineBlockingVolume.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SplineBlockingVolume.generated.h"
+
+class USplineComponent;
+class UBoxComponent;
+
+UCLASS()
+class DOMINIONPROTOCOL_API ASplineBlockingVolume : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASplineBlockingVolume();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Blocking Volume")
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blocking Volume")
+	TObjectPtr<USplineComponent> SplineComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blocking Volume")
+	float MaxSpacing = 100.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blocking Volume")
+	float MinSpacing = 50.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blocking Volume")
+	float CurvatureMax = 0.1f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blocking Volume")
+	FVector CollisionBoxExtent = FVector(50.f, 10.f, 400.f);
+
+private:
+	UPROPERTY()
+	TArray<UBoxComponent*> BoxComponents;
+
+	void ClearBox();
+	void CreateBox();
+	float CalculateCurvatureAtDistance(float Distance) const;
+};
