990bc5151bea6a45b9312450a0729924456e763e
seunghyeonL <hkr0416@gmail.com>
2025-05-29 21:57:34 +0900

    [add] BaseAIParriedEffect

---
+++ Source/DominionProtocol/Components/StatusComponent/StatusEffects/AIParriedEffect/BaseAIParriedEffect.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StatusComponent/StatusEffects/StatusEffectBase.h"
+#include "BaseAIParriedEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOMINIONPROTOCOL_API UBaseAIParriedEffect : public UStatusEffectBase
+{
+	GENERATED_BODY()
+};
