81d316d7aa1c6ec25611d1882db3e0e400e95a53
eomcri <jihun.eomcri@gmail.com>
2025-05-29 19:26:38 +0900

    [add] Axe WeaponSkill 추가

---
+++ Source/DominionProtocol/Components/SkillComponent/Skills/WeaponFirstAxe.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponFirstAxe.h"
+#include "Util/GameTagList.h"
+
+UWeaponFirstAxe::UWeaponFirstAxe()
+{
+	SkillTag = SkillTags::WeaponSkillAxe1;
+}
