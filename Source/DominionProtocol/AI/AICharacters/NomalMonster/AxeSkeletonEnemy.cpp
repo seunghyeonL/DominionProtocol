50102ba29c723a864c6164275c67c3f72cd38e8a
jch0422 <jeongchanhyotr@gmail.com>
2025-06-02 11:31:23 +0900

    [add] 엘리트몬스터 마이그레이션, 일반몬스터 ABP생성

---
+++ Source/DominionProtocol/AI/AICharacters/NomalMonster/AxeSkeletonEnemy.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/AICharacters/NomalMonster/AxeSkeletonEnemy.h"
+
+AAxeSkeletonEnemy::AAxeSkeletonEnemy()
+{
+	PawnTag = PawnTags::AxeSkeleton;
+}
