1464da1300e8f6f831a470cc875ddfe9049aae93
ChaeKH <rbgur211@naver.com>
2025-06-02 18:41:11 +0900

    [add] DropEssence code for OnPlayerDeath
    플레이어 사망 시 레벨 상에 떨구는 균열 정수 코드 추가

---
+++ Source/DominionProtocol/WorldObjects/DropEssence.cpp
@@ -0,0 +1,99 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DropEssence.h"
+#include "Components/SphereComponent.h"
+#include "DomiFramework/GameInstance/DomiGameInstance.h"
+#include "DomiFramework/GameInstance/WorldInstanceSubsystem.h"
+
+#include "Util/DebugHelper.h"
+
+ADropEssence::ADropEssence()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	StaticMeshComp->SetupAttachment(SceneRoot);
+	StaticMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("InteractRadiusSphereComponent");
+	SphereComponent->SetupAttachment(SceneRoot);
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ADropEssence::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ADropEssence::OnOverlapBegin);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ADropEssence::OnOverlapEnd);
+
+	GameInstance = GetGameInstance<UDomiGameInstance>();
+	if (IsValid(GameInstance))
+	{
+		WorldInstanceSubsystem = GameInstance->GetSubsystem<UWorldInstanceSubsystem>();
+	}
+	check(GameInstance);
+	check(WorldInstanceSubsystem);
+
+	WorldInstanceSubsystem->SetFieldEssenceAmount(GameInstance->GetPlayerCurrentEssence());
+}
+
+void ADropEssence::Interact_Implementation(AActor* Interactor)
+{
+	if (!IsValid(GameInstance))
+	{
+		GameInstance = GetGameInstance<UDomiGameInstance>();
+	}
+	if (!IsValid(WorldInstanceSubsystem))
+	{
+		WorldInstanceSubsystem = GameInstance->GetSubsystem<UWorldInstanceSubsystem>();
+	}
+	
+	check(GameInstance);
+	check(WorldInstanceSubsystem);
+
+	GameInstance->SetPlayerCurrentEssence(GameInstance->GetPlayerCurrentEssence() + WorldInstanceSubsystem->GetFieldEssenceAmount());
+	//나중에 복구 UI 만들어지면 그걸로 변경
+	Debug::Print(TEXT("Essence Restored!!"));
+	
+	Destroy();
+}
+
+FText ADropEssence::GetInteractMessage_Implementation() const
+{
+	return IInteractableInterface::GetInteractMessage_Implementation();
+}
+
+void ADropEssence::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsValid(OtherActor) && !OtherActor->ActorHasTag("Player"))
+	{
+		return;
+	}
+
+	if (!IsValid(PlayerCharacter))
+	{
+		PlayerCharacter = Cast<ADomiCharacter>(OtherActor);
+	}
+
+	if (IsValid(PlayerCharacter))
+	{
+		PlayerCharacter->AddInteractableActor(this);
+	}
+}
+
+void ADropEssence::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (IsValid(OtherActor) && IsValid(PlayerCharacter) && OtherActor == PlayerCharacter)
+	{
+		PlayerCharacter->RemoveInteractableActor(this);
+	}
+}
---
+++ Source/DominionProtocol/WorldObjects/DropEssence.h
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractableInterface.h"
+#include "Player/Characters/DomiCharacter.h"
+#include "DropEssence.generated.h"
+
+class UWorldInstanceSubsystem;
+class UDomiGameInstance;
+class USphereComponent;
+
+UCLASS()
+class DOMINIONPROTOCOL_API ADropEssence : public AActor, public IInteractableInterface
+{
+	GENERATED_BODY()
+
+//Functions
+public:
+	ADropEssence();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	virtual void Interact_Implementation(AActor* Interactor) override;
+
+	virtual FText GetInteractMessage_Implementation() const override;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+//Variables
+protected:
+	//Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> StaticMeshComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+
+private:
+	UPROPERTY()
+	TObjectPtr<ADomiCharacter> PlayerCharacter;
+
+	UPROPERTY()
+	TObjectPtr<UDomiGameInstance> GameInstance;
+
+	UPROPERTY()
+	TObjectPtr<UWorldInstanceSubsystem> WorldInstanceSubsystem;
+	
+	int32 EssenceAmount;
+};
