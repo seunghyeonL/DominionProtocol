47e54fd86ce700c9dd727fb17f72745012cef6f5
ChaeKH <rbgur211@naver.com>
2025-06-02 18:40:21 +0900

    [add] Essence Actor code for Monster::Ondeath

---
+++ Source/DominionProtocol/WorldObjects/Essence.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Essence.h"
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "DomiFramework/GameInstance/DomiGameInstance.h"
+#include "Player/Characters/DomiCharacter.h"
+
+#include "Util/DebugHelper.h"
+
+AEssence::AEssence()
+	:	MaxSpeed(1500.f),
+		InitialSpeed(0.f),
+		Accelerate(10),
+		CurrentSpeed(InitialSpeed)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+
+	NaigaraComponent = CreateDefaultSubobject<UNiagaraComponent>("NaigaComponent");
+	NaigaraComponent->SetupAttachment(SceneRoot);
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SphereComponent->SetupAttachment(SceneRoot);
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void AEssence::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SphereComponent->OnComponentHit.AddDynamic(this, &AEssence::OnHitWithPlayer);
+
+	PlayerCharacter = Cast<ADomiCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	checkf(PlayerCharacter, TEXT("AEssence::BeginPlay : DomiCharacter Not Found"));
+}
+
+void AEssence::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!IsValid(PlayerCharacter))
+	{
+		Debug::PrintError(TEXT("AEssence::Tick : DomiCharacter is Invalid"));
+		return;
+	}
+	
+	FVector NewPlayerLocation = PlayerCharacter->GetActorLocation();
+	FVector NewDirection = (NewPlayerLocation - GetActorLocation()).GetSafeNormal();
+	
+	CurrentSpeed += Accelerate * DeltaTime;
+	CurrentSpeed = FMath::Clamp(CurrentSpeed, 0.f, MaxSpeed);
+
+	FVector Movement = NewDirection * CurrentSpeed * DeltaTime;
+}
+
+void AEssence::OnHitWithPlayer(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!IsValid(OtherActor) && !OtherActor->ActorHasTag("Player"))
+	{
+		return;
+	}
+
+	if (OtherActor == PlayerCharacter)
+	{
+		if (EssenceAmount != 0)
+		{
+			UDomiGameInstance* GameInstance = GetGameInstance<UDomiGameInstance>();
+			if (IsValid(GameInstance))
+			{
+				GameInstance->AddPlayerCurrentEssence(EssenceAmount);
+			}
+			Destroy();
+		}
+		else
+		{
+			Debug::PrintError(TEXT("AEssence::OnHitWithPlayer : EssenceAmount Set is missing"));
+		}
+	}
+}
---
+++ Source/DominionProtocol/WorldObjects/Essence.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Essence.generated.h"
+
+class ADomiCharacter;
+class UProjectileMovementComponent;
+class USphereComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class DOMINIONPROTOCOL_API AEssence : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEssence();
+
+	void SetEssenceAmount(int32 InEssenceAmount) { EssenceAmount = InEssenceAmount; }
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnHitWithPlayer(
+		UPrimitiveComponent* HitComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	TObjectPtr<UNiagaraComponent> NaigaraComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Property")
+	float MaxSpeed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Property")
+	float InitialSpeed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Property")
+	int32 Accelerate;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<ADomiCharacter> PlayerCharacter;
+	
+	int32 EssenceAmount;
+
+	float CurrentSpeed;
+};
