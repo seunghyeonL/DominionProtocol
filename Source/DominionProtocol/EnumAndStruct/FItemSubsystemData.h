1e742d7af36aeab6256a631bd6700812c4b11ff2
ChaeKH <rbgur211@naver.com>
2025-06-06 00:02:33 +0900

    [fix] instance Save Data struct fix

--- Source/DominionProtocol/EnumAndStruct/FItemSubsystemData.h
+++ Source/DominionProtocol/EnumAndStruct/FItemSubsystemData.h
@@ -3,10 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Util/GameTagList.h"
 #include "FItemSubsystemData.generated.h"
 
 USTRUCT(BlueprintType)
 struct FItemSubsystemData
 {
 	GENERATED_BODY()
+
+	FItemSubsystemData()
+		:	bIsPotionBoostApplied(false)
+	{}
+	
+	UPROPERTY()
+	TMap<FGameplayTag, int32> InventoryMap;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Equipment", meta = (AllowPrivateAccess = "true"))
+	TMap<FName, FGameplayTag> EquipmentSlots;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Consumable")
+	TMap<FName, FGameplayTag> ConsumableSlots;
+
+	UPROPERTY()
+	bool bIsPotionBoostApplied;
 };
