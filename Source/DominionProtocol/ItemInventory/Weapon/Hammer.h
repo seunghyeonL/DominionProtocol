eccd529d2cccd27f9fd92cb0f9f3380fd5093421
ChungSH <kevin990114@gmail.com>
2025-06-05 09:41:00 +0900

    [add] 스킬, 무기, 장신구 아이템 클래스 추가 생성 & 드롭 클래스 추가

---
+++ Source/DominionProtocol/ItemInventory/Weapon/Hammer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemInventory/BaseItem.h"
+#include "Hammer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOMINIONPROTOCOL_API AHammer : public ABaseItem
+{
+	GENERATED_BODY()
+	
+};
