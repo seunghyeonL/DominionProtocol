ed214a6c68b771461acce461308a7f98104106a0
김서영 <ksy99921@gmail.com>
2025-06-07 02:28:08 +0900

    [fix] Add forced story state change

--- Source/DominionProtocol/WorldObjects/StoryTrigger.cpp
+++ Source/DominionProtocol/WorldObjects/StoryTrigger.cpp
@@ -1,13 +1,75 @@
 #include "WorldObjects/StoryTrigger.h"
+#include "Components/BoxComponent.h"
+#include "DomiFramework/GameInstance/DomiGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "Util/DebugHelper.h"
+#include "Player/Characters/DomiCharacter.h"
 
 AStoryTrigger::AStoryTrigger()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->SetupAttachment(RootComponent);
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 
 }
 
 void AStoryTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AStoryTrigger::OnOverlapBegin);
+	CollisionBox->OnComponentEndOverlap.AddDynamic(this, &AStoryTrigger::OnOverlapEnd);
+
+}
+
+void AStoryTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Debug::Print(TEXT("AStoryTrigger: Overlap"));
+
+	if (!IsValid(OtherActor) || !OtherActor->ActorHasTag("Player"))
+	{
+		Debug::Print(TEXT("Not Player"));
+		return;
+	}
+
+	ADomiCharacter* PlayerCharacter = Cast<ADomiCharacter>(OtherActor);
+	ensure(PlayerCharacter);
+	CachedCharacter = PlayerCharacter;
+
+	PlayerCharacter->AddInteractableActor(this);
+}
+
+void AStoryTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (IsValid(OtherActor) && OtherActor == CachedCharacter)
+	{
+		ADomiCharacter* PlayerCharacter = Cast<ADomiCharacter>(OtherActor);
+		ensure(PlayerCharacter);
+
+		CachedCharacter = nullptr;
+		PlayerCharacter->RemoveInteractableActor(this);
+	}
+	else
+	{
+		Debug::Print(TEXT("AStoryTrigger::OnOverlapEnd : OtherActor Is not PlayerCharacter"));
+	}
+}
+
+void AStoryTrigger::Interact_Implementation(AActor* Interactor)
+{
+	ADomiCharacter* PlayerCharacter = Cast<ADomiCharacter>(Interactor);
+	if (!PlayerCharacter) return;
+
+	UDomiGameInstance* GI = Cast<UDomiGameInstance>(UGameplayStatics::GetGameInstance(this));
+
+	GI->SetCurrentGameStoryState(ForcedStoryState);
+}
+
+FText AStoryTrigger::GetInteractMessage_Implementation() const
+{
+	return FText::FromString(TEXT("Story 확인하기"));
 }
--- Source/DominionProtocol/WorldObjects/StoryTrigger.h
+++ Source/DominionProtocol/WorldObjects/StoryTrigger.h
@@ -6,8 +6,11 @@
 #include "Interface/InteractableInterface.h"
 #include "StoryTrigger.generated.h"
 
+class UBoxComponent;
+class ADomiCharacter;
+
 UCLASS()
-class DOMINIONPROTOCOL_API AStoryTrigger : public AActor
+class DOMINIONPROTOCOL_API AStoryTrigger : public AActor, public IInteractableInterface
 {
 	GENERATED_BODY()
 	
@@ -17,4 +20,33 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	virtual void Interact_Implementation(AActor* Interactor) override;
+	virtual FText GetInteractMessage_Implementation() const override;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UBoxComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere)
+	EGameStoryState ForcedStoryState;
+
+private:
+	UPROPERTY()
+	ADomiCharacter* CachedCharacter;
 };
