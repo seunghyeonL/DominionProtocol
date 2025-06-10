76d007603ead856ee3e8b46d044a318bc36e6277
eomcri <jihun.eomcri@gmail.com>
2025-06-11 04:47:29 +0900

    [add] Boss2 DashAttack, DashAttackB 소스 파일 추가

---
+++ Source/DominionProtocol/Components/SkillComponent/Skills/BossSkill/Boss2/DashAttackB/Boss2DashAttackBFirst.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Boss2DashAttackBFirst.h"
+
+UBoss2DashAttackBFirst::UBoss2DashAttackBFirst()
+{
+	SkillTag = SkillTags::Boss2DashAttackBFirst;
+}
