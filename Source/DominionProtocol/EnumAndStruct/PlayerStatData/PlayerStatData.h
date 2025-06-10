3cc2f7b40731d5f4cc61d093639b3dd21110534a
seunghyeonL <hkr0416@gmail.com>
2025-06-10 15:38:59 +0900

    [add] PlayerStatData struct

---
+++ Source/DominionProtocol/EnumAndStruct/PlayerStatData/PlayerStatData.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "PlayerStatData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPlayerStatData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Str;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Life;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Dex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float End;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Weapon1AttackPower;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Weapon2AttackPower;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpeedCoefficient;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float StaminaRecoveryRate; 
+};
