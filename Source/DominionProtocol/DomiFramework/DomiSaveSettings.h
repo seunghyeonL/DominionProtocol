3393c0b95948e792f12fd89a6ed6727a44f2ba47
ChaeKH <rbgur211@naver.com>
2025-06-11 13:47:31 +0900

    [add] New SaveGame Class for Settings

---
+++ Source/DominionProtocol/DomiFramework/DomiSaveSettings.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnumAndStruct/FSoundSubsystemData.h"
+#include "GameFramework/SaveGame.h"
+#include "DomiSaveSettings.generated.h"
+
+UCLASS()
+class DOMINIONPROTOCOL_API UDomiSaveSettings : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	FSoundSubsystemData SoundSubsystemData;
+};
