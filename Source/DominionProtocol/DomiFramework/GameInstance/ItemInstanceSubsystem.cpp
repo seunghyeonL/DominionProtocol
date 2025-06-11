a06e4c28d76899bc9584eef410b29d8761737e4f
ChaeKH <rbgur211@naver.com>
2025-06-11 12:53:43 +0900

    [update] ItemInstanceSubsystem : Add code for Save&Load
    기존 비어있던 Save&Load 함수 작성

--- Source/DominionProtocol/DomiFramework/GameInstance/ItemInstanceSubsystem.cpp
+++ Source/DominionProtocol/DomiFramework/GameInstance/ItemInstanceSubsystem.cpp
@@ -2,17 +2,28 @@
 
 
 #include "ItemInstanceSubsystem.h"
+#include "Player/Characters/DomiCharacter.h"
+#include "Components/ItemComponent/ItemComponent.h"
 
 #include "Util/DebugHelper.h"
 
 void UItemInstanceSubsystem::LoadSaveData(const FItemSubsystemData& SaveData)
 {
+	InventoryDataMap = SaveData.InventoryMap;
+	EquipmentSlotMap = SaveData.EquipmentSlots;
+	ConsumableSlotMap = SaveData.ConsumableSlots;
+	bIsPotionBoostApplied = SaveData.bIsPotionBoostApplied;
 }
 
 FItemSubsystemData UItemInstanceSubsystem::GetSaveData() const
 {
 	FItemSubsystemData SaveData;
 
+	SaveData.InventoryMap = InventoryDataMap;
+	SaveData.EquipmentSlots = EquipmentSlotMap;
+	SaveData.ConsumableSlots = ConsumableSlotMap;
+	SaveData.bIsPotionBoostApplied = bIsPotionBoostApplied;
+	
 	return SaveData;
 }
 
