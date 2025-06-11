36aa258678bbf27bcf0c5fe61516d1a9dc4f75ce
ChaeKH <rbgur211@naver.com>
2025-06-11 12:51:09 +0900

    [fix] WorldInstanceSubsystemData Struct update for Save&Load
    월드인스턴스서브시스템의 저장&불러오기를 위한 구조체 내부 데이터 수정

--- Source/DominionProtocol/EnumAndStruct/FWorldInstanceSubsystemData.h
+++ Source/DominionProtocol/EnumAndStruct/FWorldInstanceSubsystemData.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "FWorldActorData.h"
+#include "FCrackData.h"
+#include "WorldObjects/DropEssence.h"
 #include "FWorldInstanceSubsystemData.generated.h"
 
 USTRUCT(BlueprintType)
@@ -11,4 +13,39 @@ struct FWorldInstanceSubsystemData
 {
 	GENERATED_BODY()
 
+	UPROPERTY()
+	TMap<FString, bool> IsNewGameDataMap;
+	
+	UPROPERTY()
+	FString CurrentLevelName;
+
+	UPROPERTY()
+	FText CurrentLevelDisplayName;
+
+	UPROPERTY()
+	FText RecentCrackName;
+
+	UPROPERTY()
+	int32 RecentCrackIndex = 0;
+
+	UPROPERTY()
+	int32 MaxCrackIndex = 1;
+
+	UPROPERTY()
+	TMap<FString, FCrackDataArrayStruct> CrackDataMap;
+
+	UPROPERTY()
+	TMap<FGuid, FWorldActorData> WorldActorDataMap;
+	
+	UPROPERTY()
+	bool bIsDropEssenceExist;
+	
+	UPROPERTY()
+	int32 DropEssenceAmount;
+
+	UPROPERTY()
+	FVector DropEssenceLocation;
+
+	UPROPERTY()
+	FString DropEssenceLocationLevel;
 };
