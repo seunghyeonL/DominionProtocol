50e141f459391cd0c66adcf646014d3ab73af694
seunghyeonL <hkr0416@gmail.com>
2025-06-16 17:33:49 +0900

    [add] EPlayerMoveState

---
+++ Source/DominionProtocol/EnumAndStruct/PlayerMoveState.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerMoveState.generated.h"
+
+UENUM(BlueprintType)
+enum class EPlayerMoveState : uint8
+{
+	Walking UMETA(DisplayName = "Walking"),
+	Running UMETA(DisplayName = "Running"),
+	Sprinting UMETA(DisplayName = "Sprinting"),
+	MAX UMETA(DisplayName = "MAX")
+};
