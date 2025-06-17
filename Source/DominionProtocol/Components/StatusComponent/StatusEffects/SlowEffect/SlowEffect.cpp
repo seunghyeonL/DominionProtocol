dd4ebc069cd326522aee921836ef91ef3d3ba279
seunghyeonL <hkr0416@gmail.com>
2025-06-17 20:09:19 +0900

    [add] SlowEffect

--- Source/DominionProtocol/Components/StatusComponent/StatusEffects/SlowEffect/SlowEffect.cpp
+++ Source/DominionProtocol/Components/StatusComponent/StatusEffects/SlowEffect/SlowEffect.cpp
@@ -2,3 +2,52 @@
 
 
 #include "SlowEffect.h"
+
+#include "Components/StatusComponent/StatusComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+USlowEffect::USlowEffect()
+{
+	StatusEffectTag = EffectTags::Slow;
+}
+
+bool USlowEffect::Activate()
+{
+	if (!Super::Activate())
+	{
+		return false;
+	}
+	
+	check(OwnerCharacter);
+
+	auto MovementComponent = Cast<UCharacterMovementComponent>(OwnerCharacter->GetMovementComponent());
+	MovementComponent->MaxWalkSpeed /= Magnitude;
+	return true;
+}
+
+bool USlowEffect::Activate(float Duration)
+{
+	if (!Super::Activate(Duration))
+	{
+		return false;
+	}
+	
+	check(OwnerCharacter);
+
+	auto MovementComponent = Cast<UCharacterMovementComponent>(OwnerCharacter->GetMovementComponent());
+	MovementComponent->MaxWalkSpeed /= Magnitude;
+	return true;
+}
+
+void USlowEffect::Deactivate()
+{
+	if (!bIsActive) return;
+	
+	Super::Deactivate();
+
+	check(OwnerCharacter);
+
+	auto MovementComponent = Cast<UCharacterMovementComponent>(OwnerCharacter->GetMovementComponent());
+	MovementComponent->MaxWalkSpeed *= Magnitude;
+}
