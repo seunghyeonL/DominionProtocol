ce5c274aaf87b41305df4b7ee0129ddae205bc19
김서영 <ksy99921@gmail.com>
2025-06-18 18:32:01 +0900

    [fix] DialogueData: Allow progression up to Boss 4

--- Source/DominionProtocol/UI/UIInGame/NewDialogueWidget.cpp
+++ Source/DominionProtocol/UI/UIInGame/NewDialogueWidget.cpp
@@ -5,6 +5,7 @@
 
 #include "Kismet/GameplayStatics.h"
 #include "WorldObjects/Crack.h"
+#include "WorldObjects/StoryTrigger.h"
 #include "WorldObjects/DialogueManager.h"
 
 void UNewDialogueWidget::UpdateDialogueWidget(const FText NewText)
@@ -29,6 +30,15 @@ void UNewDialogueWidget::BindCreateDialogueDelegate()
 	{
 		auto* Crack = Cast<ACrack>(Actor);
 		Crack->OnCreateDialogueManager.AddUObject(this, &UNewDialogueWidget::BindDialogueDelegate);
+	}
+
+	TArray<AActor*> TriggerActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AStoryTrigger::StaticClass(), TriggerActors);
+
+	for (const auto Actor : TriggerActors)
+	{
+		auto* StoryTrigger = Cast<AStoryTrigger>(Actor);
+		StoryTrigger->OnCreateDialogueManager.AddUObject(this, &UNewDialogueWidget::BindDialogueDelegate);
 	}
 }
 
