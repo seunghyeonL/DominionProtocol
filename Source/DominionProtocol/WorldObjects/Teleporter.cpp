5812174fd8cc7061839501e3a13f43f6dda293f8
ChaeKH <rbgur211@naver.com>
2025-06-18 13:57:56 +0900

    [add] Teleporter actor
    서로 연결된 상태로 상호작용시 서로의 위치로 이동하는 텔레포터 추가

---
+++ Source/DominionProtocol/WorldObjects/Teleporter.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Teleporter.h"
+#include "Components/SphereComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Player/Characters/DomiCharacter.h"
+
+#include "Util/DebugHelper.h"
+
+
+ATeleporter::ATeleporter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(SceneComp);
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(SceneComp);
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	ArrowComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+	ArrowComponent->SetupAttachment(SceneComp);
+}
+
+void ATeleporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ATeleporter::OnOverlapBegin);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ATeleporter::OnOverlapEnd);
+}
+
+void ATeleporter::Interact_Implementation(AActor* Interactor)
+{
+	if (IsValid(CachedCharacter))
+	{
+		CachedCharacter->TeleportTo(LinkedTeleporter->GetActorLocation(), LinkedTeleporter->GetActorRotation());
+	}
+	else
+	{
+		Debug::Print(TEXT("ATeleporter::Interact : CachedCharacter is invalid"));
+	}
+}
+
+FText ATeleporter::GetInteractMessage_Implementation() const
+{
+	return FText::FromString(TEXT("순간이동"));
+}
+
+void ATeleporter::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsValid(OtherActor) || !OtherActor->ActorHasTag("Player"))
+	{
+		return;
+	}
+
+	if (!IsValid(CachedCharacter))
+	{
+		ADomiCharacter* PlayerCharacter = Cast<ADomiCharacter>(OtherActor);
+		ensure(PlayerCharacter);
+		CachedCharacter = PlayerCharacter;
+	}
+
+	CachedCharacter->AddInteractableActor(this);
+}
+
+void ATeleporter::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if (IsValid(OtherActor) && OtherActor == CachedCharacter)
+	{
+		if (!IsValid(CachedCharacter))
+		{
+			ADomiCharacter* PlayerCharacter = Cast<ADomiCharacter>(OtherActor);
+			ensure(PlayerCharacter);
+			CachedCharacter = PlayerCharacter;
+		}
+		
+		CachedCharacter->RemoveInteractableActor(this);
+	}
+}
---
+++ Source/DominionProtocol/WorldObjects/Teleporter.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractableInterface.h"
+#include "Teleporter.generated.h"
+
+class UArrowComponent;
+class ADomiCharacter;
+class USphereComponent;
+
+UCLASS()
+class DOMINIONPROTOCOL_API ATeleporter : public AActor, public IInteractableInterface
+{
+	GENERATED_BODY()
+
+// Functions
+public:
+	ATeleporter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Interact_Implementation(AActor* Interactor) override;
+	virtual FText GetInteractMessage_Implementation() const override;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+	
+// Variables
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TObjectPtr<USceneComponent> SceneComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UArrowComponent> ArrowComponent;
+	
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite)
+	TObjectPtr<ATeleporter> LinkedTeleporter;
+
+private:
+	TObjectPtr<ADomiCharacter> CachedCharacter;
+};
