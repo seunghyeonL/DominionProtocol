4b2a54abadfef4b13d36d9355e9bb2f86e48a47f
seunghyeonL <hkr0416@gmail.com>
2025-06-22 15:04:33 +0900

    [add] PhysicalMaterials, SurfaceTypeDataTable

---
+++ Source/DominionProtocol/EnumAndStruct/PhysicalSurfaceTypeData/PhysicalSurfaceTypeData.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NiagaraSystem.h"
+#include "PhysicalSurfaceTypeData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPhysicalSurfaceTypeData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<USoundBase> FootStepSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<USoundBase> HitSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UNiagaraSystem> FootStepVfx;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UNiagaraSystem> HitVfx;
+};
