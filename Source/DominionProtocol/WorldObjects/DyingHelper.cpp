968a68ce14987d130ad3e0687845b40882963d9a
ChaeKH <rbgur211@naver.com>
2025-06-23 10:13:12 +0900

    [add] DyingHelper

---
+++ Source/DominionProtocol/WorldObjects/DyingHelper.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DyingHelper.h"
+#include "ItemInventory/ItemDropped.h"
+#include "WorldObjects/DialogueManager.h"
+
+#include "Util/DebugHelper.h"
+
+
+ADyingHelper::ADyingHelper()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Hair = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("HairMesh"));
+	Hair->SetupAttachment(GetMesh());
+	
+	GetMesh()->bReceivesDecals = false;
+}
+
+void ADyingHelper::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DialogueManager = NewObject<UDialogueManager>(this);
+}
+
+void ADyingHelper::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	DialogueManager == nullptr;
+	Super::EndPlay(EndPlayReason);
+}
+
+void ADyingHelper::Interact_Implementation(AActor* Interactor)
+{
+	//DialogueManager
+}
+
+FText ADyingHelper::GetInteractMessage_Implementation() const
+{
+	return FText::FromString(TEXT("대화하기"));
+}
+
+void ADyingHelper::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ADyingHelper::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+}
+
+void ADyingHelper::Die()
+{
+	// 아이템 드랍
+	if (IsValid(ItemDroppedClass.Get()))
+	{
+		GetWorld()->SpawnActor<AItemDropped>(ItemDroppedClass.Get(), GetActorLocation(), GetActorRotation());
+	}
+
+	GetWorldTimerManager().SetTimer(
+		DieTimerHandle,
+		[this]()
+		{
+			Destroy();
+		},
+		3.f,
+		false);
+}
---
+++ Source/DominionProtocol/WorldObjects/DyingHelper.h
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Interface/InteractableInterface.h"
+#include "DyingHelper.generated.h"
+
+class AItemDropped;
+class UDialogueManager;
+
+UCLASS()
+class DOMINIONPROTOCOL_API ADyingHelper : public ACharacter, public IInteractableInterface
+{
+	GENERATED_BODY()
+
+public:
+	ADyingHelper();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	virtual void Interact_Implementation(AActor* Interactor) override;
+	virtual FText GetInteractMessage_Implementation() const override;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+	
+private:
+	void Die();
+
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animation")
+	TObjectPtr<UAnimMontage> IdleMontage;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animation")
+	TObjectPtr<UAnimMontage> DieMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftClassPtr<AItemDropped> ItemDroppedClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USkeletalMeshComponent> Hair;
+
+	UPROPERTY()
+	TObjectPtr<UDialogueManager> DialogueManager;
+
+private:
+	FTimerHandle DieTimerHandle;
+	
+};
